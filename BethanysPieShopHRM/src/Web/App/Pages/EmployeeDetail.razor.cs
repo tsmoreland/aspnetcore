661891f49184f574a915bb30a67d166ae111d1d4
Terry Moreland <tsmoreland@live.com>
2022-11-06 23:05:17 +0000

    move Map Component to separate component library

--- BethanysPieShopHRM/src/Web/App/Pages/EmployeeDetail.razor.cs
+++ BethanysPieShopHRM/src/Web/App/Pages/EmployeeDetail.razor.cs
@@ -1,4 +1,5 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using BethanysPieShopHRM.Shared.Models;
 using BethanysPieShopHRM.Web.App.Services;
 using Microsoft.AspNetCore.Components;
 
@@ -14,9 +15,18 @@ public partial class EmployeeDetail
     [Inject]
     public IEmployeeDataService EmployeeDataService { get; set; } = default!;
 
+    public List<Marker> MapMarkers { get; set; } = default!;
+
     /// <inheritdoc />
     protected override async Task OnInitializedAsync()
     {
         Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        if (Employee is { Longitude: { }, Latitude: { } })
+        {
+            MapMarkers = new List<Marker>
+            {
+                new(Employee.FullName, Employee.Longitude.Value, Employee.Latitude.Value, false),
+            };
+        }
     }
 }
