fcb48ce3e1120f1ec53d71ac859b64507804e9b8
Terry Moreland <tsmoreland@live.com>
2022-11-07 22:49:39 +0000

    add razor component to show login/logout controls

---
+++ BethanysPieShopHRM/src/Web/App/Components/AuthenticationStatus.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace BethanysPieShopHRM.Web.App.Components;
+
+public partial class AuthenticationStatus
+{
+    [Inject]
+    public NavigationManager Navigation { get; set; } = default!;
+
+    [Inject]
+    public SignOutSessionStateManager SessionManager { get; set; } = default!;
+
+    private async Task BeginSignOut(MouseEventArgs e)
+    {
+        _ = e;
+        await SessionManager.SetSignOutState();
+        Navigation.NavigateTo("authentication/login");
+    }
+}
