aab6c6f8f29ab3e340c3b294bcc823ae27ed108d
Terry Moreland <tsmoreland@live.com>
2022-11-07 22:29:36 +0000

    update index.html and program to add authentication to services (and reference the service script)

--- BethanysPieShopHRM/src/Web/App/Program.cs
+++ BethanysPieShopHRM/src/Web/App/Program.cs
@@ -12,7 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-builder.Services.AddScoped<ApplicationState>();
-builder.Services.AddBlazoredLocalStorage();
+builder.Services
+    .AddScoped<ApplicationState>()
+    .AddBlazoredLocalStorage()
+    .AddOidcAuthentication(options =>
+    {
+        builder.Configuration.Bind("Auth0", options.ProviderOptions);
+        options.ProviderOptions.ResponseType = "code";
+    });
 
 await builder.Build().RunAsync();
