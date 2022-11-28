8fbfa0b60136d1acd05c820034df23ff96ed8801
Terry Moreland <tsmoreland@live.com>
2022-11-28 02:16:51 +0000

    update UI with employee overview imlementation - early implementation, no paging yet

--- WiredBrainCoffee.EmployeeManager/src/WiredBrainCoffee.EmployeeManager.Server.App/Pages/EmployeeOverview.razor.cs
+++ WiredBrainCoffee.EmployeeManager/src/WiredBrainCoffee.EmployeeManager.Server.App/Pages/EmployeeOverview.razor.cs
@@ -1,5 +1,22 @@
-﻿namespace WiredBrainCoffee.EmployeeManager.Server.App.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using WiredBrainCoffee.EmployeeManager.Domain.Contracts;
+using WiredBrainCoffee.EmployeeManager.Domain.Models;
+
+namespace WiredBrainCoffee.EmployeeManager.Server.App.Pages;
 
 public partial class EmployeeOverview
 {
+    private IReadOnlyList<Employee>? Employees { get; set; }
+
+    [Inject]
+    private IRepositoryFactory RepositoryFactory { get; set; } = null!;
+
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        await using IEmployeeRepository repository = RepositoryFactory.BuildEmployeeRepository();
+        List<Employee> employees = await repository.FindAllAsync(default).ToListAsync(default);
+        Employees = employees.AsReadOnly();
+    }
 }
