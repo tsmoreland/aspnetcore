e58de4a1f5115d7547b34d1535a2acd075ede72a
Terry Moreland <tsmoreland@live.com>
2023-02-19 01:47:03 +0000

    add ordering api project, not yet complete, still needs Configure methods for application and infrastructure

--- GloboTicket.Shop/src/Ordering/Api/Startup.cs
+++ GloboTicket.Shop/src/Ordering/Api/Startup.cs
@@ -1,21 +1,17 @@
-﻿// 
+﻿//
 // Copyright © 2023 Terry Moreland
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
 // and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
-using GloboTicket.Shop.Catalog.Api.Services;
-using GloboTicket.Shop.Catalog.Application;
-using GloboTicket.Shop.Catalog.Infrastructure;
-using GloboTicket.Shop.Shared.Contracts.Hosting;
-
-namespace GloboTicket.Shop.Catalog.Api;
+namespace GloboTicket.Shop.Ordering.Api;
 
 internal static class Startup
 {
@@ -35,13 +31,11 @@ internal static class Startup
 
         services
             .AddProblemDetails()
-            .AddSwaggerGen()
-            .AddSingleton<IHostEnvironmentFacade, HostEnvironmentFacade>()
-            .AddCatalogApplicationServices()
-            .AddCatalogInfrastructure();
+            .AddSwaggerGen();
 
         return builder;
     }
+
 
     public static WebApplication Configure(this WebApplication app)
     {
