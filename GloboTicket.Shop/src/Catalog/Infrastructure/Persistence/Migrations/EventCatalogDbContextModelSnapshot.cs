5d78792d496e4ae9abdd760a91656c454b78f5d3
Terry Moreland <tsmoreland@live.com>
2023-03-02 00:16:13 +0000

    update targeted sdk to 7.0.3

--- GloboTicket.Shop/src/Catalog/Infrastructure/Persistence/Migrations/EventCatalogDbContextModelSnapshot.cs
+++ GloboTicket.Shop/src/Catalog/Infrastructure/Persistence/Migrations/EventCatalogDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace GloboTicket.Shop.Catalog.Infrastructure.Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.2")
+                .HasAnnotation("ProductVersion", "7.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
