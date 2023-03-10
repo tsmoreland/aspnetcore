6062101e4c93f1b585e408dc71d6137c8dec60bb
Terry Moreland <terry.s.moreland@gmail.com>
2023-03-10 11:32:11 +0000

    initial libraries and entities

---
+++ CarvedRock.EFCore/src/Domain/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace CarvedRock.Domain.Entities;
+
+public sealed class Customer
+{
+    public Customer(string firstName, string lastName, string username)
+        : this(0, firstName, lastName, username, new HashSet<Order>())
+    {
+    }
+
+    private Customer(int id, string firstName, string lastName, string username, ICollection<Order> orders)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Username = username;
+        Orders = orders;
+    }
+
+    public required int Id { get; init; }
+
+    public required string FirstName { get; set; } 
+
+    public required string LastName { get; set; }
+
+    public required string Username { get; set; }
+
+    public ICollection<Order> Orders { get; init; } 
+
+}
---
+++ CarvedRock.EFCore/src/Domain/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿namespace CarvedRock.Domain.Entities;
+
+public sealed class Order
+{
+    public Order(string name, Customer customer)
+        : this(name, customer, new HashSet<Item>())
+    {
+    }
+
+    public Order(string name, Customer customer, ICollection<Item> items)
+        : this(0, name, customer.Id, customer, items)
+    {
+    }
+
+    private Order(int id, string name, int customerId, Customer customer, ICollection<Item> items)
+    {
+        Id = id;
+        Name = name;
+        CustomerId = customerId;
+        Customer = customer;
+        Items = items;
+    }
+
+    public required int Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required int CustomerId { get; init; }
+
+    public required Customer Customer { get; init; }
+
+    public required ICollection<Item> Items { get; init; }
+}
