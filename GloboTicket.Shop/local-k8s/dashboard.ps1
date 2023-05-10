a8aad29d1aa3b1ca488c70e0fc5f1bfe203a5de7
Terry Moreland <tsmoreland@live.com>
2023-05-10 20:35:37 +0100

    update local k8s in the hopes of resolving synk issues, further work needed

--- GloboTicket.Shop/local-k8s/dashboard.ps1
+++ GloboTicket.Shop/local-k8s/dashboard.ps1
@@ -4,6 +4,6 @@ kubectl apply -f .\cluster-role-binding.yml
 
 kubectl -n kubernetes-dashboard create token admin-user
 
-#kubectl proxy
+kubectl proxy
 
 #http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
