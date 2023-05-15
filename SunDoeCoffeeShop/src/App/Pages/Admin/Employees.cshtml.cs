605f17df66857a18110df1e71f577623f7bdbc58
Terry Moreland <terry.s.moreland@gmail.com>
2023-05-15 16:40:23 +0100

    add sundoe 'vulnerable' site, it won't be following best practices

---
+++ SunDoeCoffeeShop/src/App/Pages/Admin/Employees.cshtml.cs
@@ -0,0 +1,37 @@
+﻿//
+// Copyright © 2023 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shared;
+
+namespace SunDoeCoffeeShop.App.Pages.Admin;
+
+[Authorize(AuthenticationSchemes = CertificateAuthenticationDefaults.AuthenticationScheme, Roles = RoleName.Administrator)]
+public sealed class Employees : PageModel
+{
+    private readonly ILogger<Employees> _logger;
+
+    public Employees(ILogger<Employees> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task OnGetAsync([FromQuery] string? filter = null)
+    {
+        _ = filter;
+        await Task.CompletedTask;
+    }
+}
