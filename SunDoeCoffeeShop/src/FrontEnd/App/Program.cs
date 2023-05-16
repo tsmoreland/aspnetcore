e0c328bf7520304207d7c98d926524a2d35bbc9f
Terry Moreland <terry.s.moreland@gmail.com>
2023-05-16 11:57:05 +0100

    rename App to FrontEnd/App

---
+++ SunDoeCoffeeShop/src/FrontEnd/App/Program.cs
@@ -0,0 +1,51 @@
+//
+// Copyright © 2023 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using SunDoeCoffeeShop.App;
+using Serilog;
+using SunDoeCoffeeShop.FrontEnd.App;
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Configuring Services...");
+    WebApplicationBuilder builder = WebApplication
+        .CreateBuilder(args)
+        .ConfigureServices();
+    Log.Information("Configuring Application...");
+    WebApplication app = builder
+        .Build()
+        .Configure();
+
+    Log.Information("Starting Application...");
+    await app.RunAsync();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException")
+{
+    if (args.FirstOrDefault() == "--applicationName" && ex is HostAbortedException)
+    {
+        return; // efcore migration
+    }
+
+    // https://github.com/dotnet/runtime/issues/60600 for StopTheHostException
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown complete.");
+    await Log.CloseAndFlushAsync();
+}
