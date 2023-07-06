a5ab6ee9f65d6fc7f256cfa5a2a43b9e84934d79
Terry Moreland <tsmoreland@live.com>
2023-07-06 22:55:57 +0100

    - fix path traversal risk in image upload

--- BethanysPieShopHRM/src/Api/App/Controllers/EmployeeController.cs
+++ BethanysPieShopHRM/src/Api/App/Controllers/EmployeeController.cs
@@ -64,7 +64,12 @@ public class EmployeeController : Controller
             string imageName = employee.ImageName?.Replace("..", string.Empty).Trim() ?? string.Empty;
             if (imageName is { Length: > 0 })
             {
-                string path = $"{_webHostEnvironment.WebRootPath}\\uploads\\{employee.ImageName}";
+                string path = Path.GetFullPath($"{_webHostEnvironment.WebRootPath}\\uploads\\{employee.ImageName}");
+                if (!path.StartsWith(_webHostEnvironment.WebRootPath))
+                {
+                    return BadRequest();
+                }
+
                 using (FileStream fileStream = System.IO.File.Create(path))
                 {
                     fileStream.Write(employee.ImageContent, 0, employee.ImageContent.Length);
