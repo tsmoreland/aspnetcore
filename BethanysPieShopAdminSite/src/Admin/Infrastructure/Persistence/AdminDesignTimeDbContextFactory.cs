da7e81e698d0fa7e48ccae2448354493014f184f
Terry Moreland <tsmoreland@live.com>
2023-08-13 11:57:19 +0100

    add design time factory

---
+++ BethanysPieShopAdminSite/src/Admin/Infrastructure/Persistence/AdminDesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿//
+// Copyright © 2023 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BethanysPieShop.Admin.Infrastructure.Persistence;
+
+public sealed class AdminDesignTimeDbContextFactory : IDesignTimeDbContextFactory<AdminDbContext>
+{
+    /// <inheritdoc />
+    public AdminDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptions<AdminDbContext> options = new DbContextOptionsBuilder<AdminDbContext>()
+            .UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true",
+                static options => options.MigrationsAssembly(typeof(AdminDbContext).Assembly.FullName))
+            .Options;
+        return new AdminDbContext(options);
+    }
+}
