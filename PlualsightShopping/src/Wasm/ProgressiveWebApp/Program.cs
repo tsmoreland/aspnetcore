aa217724a2b3ba15a0b268046a87bdd2a924ec38
Terry Moreland <terry.s.moreland@gmail.com>
2023-09-26 19:14:32 +0100

    renaming wasm app to avoid conflict with default component 'App'

--- PlualsightShopping/src/Wasm/ProgressiveWebApp/Program.cs
+++ PlualsightShopping/src/Wasm/ProgressiveWebApp/Program.cs
@@ -13,7 +13,7 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PlualsightShopping.Wasm.App;
+using ProgressiveWebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- PlualsightShopping/src/Wasm/ProgressiveWebApp/wwwroot/service-worker.published.js
+++ PlualsightShopping/src/Wasm/ProgressiveWebApp/wwwroot/service-worker.published.js
@@ -36,6 +36,7 @@ async function onFetch(event) {
     let cachedResponse = null;
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache
+        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate';
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
