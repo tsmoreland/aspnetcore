7d4abc4a8a1d5406eb82169daef7b12f386bc3d4
Terry Moreland <tsmoreland@live.com>
2023-11-24 22:42:13 +0000

    add support for viewing arbitrary image streamed from 'server', still trying to get scaling working

---
+++ PhotoViewer/src/RazorComponents/wwwroot/imageJsInterop.js
@@ -0,0 +1,12 @@
+﻿async function setImageUsingStreaming(imageElementId, imageStream) {
+  const arrayBuffer = await imageStream.arrayBuffer();
+  const blob = new Blob([arrayBuffer]);
+  const url = URL.createObjectURL(blob);
+
+  const element = document.getElementById(imageElementId);
+  if (element !== undefined && element !== null) {
+    document.getElementById(imageElementId).src = url;
+  } else {
+    console.log(imageElementId + ' not found')
+  }
+} 
