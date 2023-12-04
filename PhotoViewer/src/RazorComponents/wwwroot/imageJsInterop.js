21ae128c921c9fa743158eb5ba02025c3e545c0e
Terry Moreland <tsmoreland@live.com>
2023-12-04 01:56:50 +0000

    finally have stretch semi-working.
    Need to hide or remove navmenu, leaning twowards having it semi-transparent and living on top

--- PhotoViewer/src/RazorComponents/wwwroot/imageJsInterop.js
+++ PhotoViewer/src/RazorComponents/wwwroot/imageJsInterop.js
@@ -1,12 +1,16 @@
-﻿async function setImageUsingStreaming(imageElementId, imageStream) {
-  const arrayBuffer = await imageStream.arrayBuffer();
-  const blob = new Blob([arrayBuffer]);
-  const url = URL.createObjectURL(blob);
+﻿async function setImageUsingStreaming(elementId, imageStream) {
+    const arrayBuffer = await imageStream.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
 
-  const element = document.getElementById(imageElementId);
-  if (element !== undefined && element !== null) {
-    document.getElementById(imageElementId).src = url;
-  } else {
-    console.log(imageElementId + ' not found')
-  }
+    const element = document.getElementById(elementId);
+    if (element !== undefined && element !== null) {
+        element.style.backgroundImage = `url(${url})`
+        element.style.backgroundSize = 'cover';
+        element.style.height = '100%';
+        element.style.flex = '1 1 auto';
+        console.log(element.style.background)
+    } else {
+        console.log(imageElementId + ' not found')
+    }
 } 
