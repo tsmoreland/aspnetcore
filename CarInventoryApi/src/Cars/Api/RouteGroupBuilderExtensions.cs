745d645c9faf9ccd350563d71042a5ac34ca34c0
Terry Moreland <tsmoreland@live.com>
2024-03-17 02:46:40 +0000

    polishing off api project, not fully there yet as there are some cosmetic things to fix up but far enough along to start using

--- CarInventoryApi/src/Cars/Api/RouteGroupBuilderExtensions.cs
+++ CarInventoryApi/src/Cars/Api/RouteGroupBuilderExtensions.cs
@@ -1,19 +1,23 @@
 ﻿using CarInventory.Cars.Api.Handlers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace CarInventory.Cars.Api;
 
 public static class RouteGroupBuilderExtensions
 {
-    public static RouteGroupBuilder MapCarsApi(this RouteGroupBuilder group, string routePrefix, params string[] authorizationPolicies)
+    public static RouteGroupBuilder MapCarsApi(this RouteGroupBuilder group, string routePrefix,
+        string[]? addPolicies = null, string[]? getPolicies = null, string[]? updatePolicies = null, string[]? deletePolicies = null)
     {
         CarsApiLinks links = new(routePrefix);
 
         group
-            .MapAddCar(in links, authorizationPolicies)
-            .MapGetSummaries(in links, authorizationPolicies)
-            .MapGetCarById(in links, authorizationPolicies)
-            .MapDeleteCar(in links, authorizationPolicies);
+            .MapAddCar(in links, addPolicies ?? [])
+            .MapGetSummaries(in links, getPolicies ?? [])
+            .MapGetCarById(in links, getPolicies ?? [])
+            .MapUpdateCar(in links, updatePolicies ?? [])
+            .MapDeleteCar(in links, deletePolicies ?? [])
+            .WithTags("Car Inventory CRUD");
 
         return group;
     }
