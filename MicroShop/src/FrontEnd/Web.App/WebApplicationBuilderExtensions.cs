252814b2443f4859ba31a571e52ea4c9374773a3
Terry Moreland <terry.s.moreland@gmail.com>
2024-03-19 15:32:15 +0000

    MicroShop - adding initial FrontEnd Application after some basic clean up (removing css/js libraries in favour of client side libraries and cleaning up Program.cs to use extension methods

---
+++ MicroShop/src/FrontEnd/Web.App/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Authentication;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace MicroShop.Web.App;
+
+internal static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder Configure(this WebApplicationBuilder builder)
+    {
+        IConfiguration configuration = builder.Configuration;
+        IServiceCollection services = builder.Services;
+        IHostEnvironment environment = builder.Environment;
+
+        builder.WebHost
+            .UseKestrel(static (ctx, options) =>
+            {
+                options
+                    .Configure(ctx.Configuration.GetSection("Kestrel"))
+                    .Endpoint("HTTPS", listenOptions => listenOptions.HttpsOptions.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13);
+                options.AddServerHeader = false;
+            });
+
+        services
+            .AddResponseCompression(static options =>
+            {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            })
+            .Configure<BrotliCompressionProviderOptions>(static options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+
+        services.AddControllersWithViews();
+
+        return builder;
+    }
+}
