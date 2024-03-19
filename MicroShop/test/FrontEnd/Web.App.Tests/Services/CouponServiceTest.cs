dd88794d397200b22948ee84d07fb25f1f142868
Terry Moreland <tsmoreland@live.com>
2024-03-19 23:34:06 +0000

    MicroShop - add test project for FrontEnd project

---
+++ MicroShop/test/FrontEnd/Web.App.Tests/Services/CouponServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using MicroShop.Web.App.Services;
+
+namespace MicroShop.Web.App.Tests.Services;
+
+public sealed class CouponServiceTest
+{
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_WhenBaseServiceIsNull()
+    {
+        ArgumentNullException? ex = Assert.Throws<ArgumentNullException>(() => _ = new CouponService(null!));
+        ex.ParamName.Should().Be("baseService");
+    }
+}
