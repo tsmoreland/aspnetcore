cba348931f257209f9d125c4cd2d2e0fd7014502
Terry Moreland <tsmoreland@live.com>
2024-03-20 01:10:37 +0000

    MicroShop - implemented early tests for CouponService with a bit of delayed red/green TDD - mostly red but now green

---
+++ MicroShop/test/FrontEnd/Web.App.Tests/TestDelegatingHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using MicroShop.Web.App.Models;
+using MicroShop.Web.App.Models.Coupons;
+
+namespace MicroShop.Web.App.Tests;
+
+public sealed record class TestResponse(HttpStatusCode StatusCode, object? Content, string ContentType = "application/json");
+
+public sealed class TestDelegatingHandler : DelegatingHandler
+{
+    private readonly Dictionary<MethodUrlKey, TestResponse> _responsesByMethodUrl = [];
+
+    public void AddResponse(MethodUrlKey key, TestResponse response)
+    {
+        _responsesByMethodUrl[key] = response;
+    }
+
+    public void ClearResponses()
+    {
+        _responsesByMethodUrl.Clear();
+    }
+
+
+    /// <inheritdoc />
+    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        MethodUrlKey key = new(request.Method, request.RequestUri);
+        HttpResponseMessage response = new();
+
+        if (!_responsesByMethodUrl.TryGetValue(key, out TestResponse? responseData))
+        {
+            return response;
+        }
+
+        response.StatusCode = responseData.StatusCode;
+
+        string jsonData = responseData.Content is not null
+            ? JsonSerializer.Serialize(responseData.Content)
+            : JsonSerializer.Serialize(new ResponseDto<object>(null, false, "-- response not present in dictionary --"));
+
+        response.Content = new StringContent(jsonData, Encoding.UTF8, responseData.ContentType);
+        return response;
+    }
+
+    /// <inheritdoc />
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(Send(request, cancellationToken));
+    }
+}
+
