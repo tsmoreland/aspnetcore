f1209cabe35ed38fcb92eecf61c9c593a1f435fd
Terry Moreland <tsmoreland@live.com>
2024-03-20 01:09:51 +0000

    MicroShop - removed ResponseDto leaving only ResponseDto<T>

--- MicroShop/src/FrontEnd/Web.App/Models/ResponseDto.cs
+++ MicroShop/src/FrontEnd/Web.App/Models/ResponseDto.cs
@@ -1,16 +1,3 @@
 ﻿namespace MicroShop.Web.App.Models;
 
-public sealed record class ResponseDto(object? Data, bool Success = true, string? ErrorMessage = null) : ResponseDto<object>(Data, Success, ErrorMessage)
-{
-    public ResponseDto<T> ToTypedResponseOrThrow<T>()
-    {
-        return Data switch
-        {
-            null => new ResponseDto<T>(default, Success, ErrorMessage),
-            T data => new ResponseDto<T>(data, Success, ErrorMessage),
-            _ => throw new InvalidCastException($"Unable to convert to RespponseDto<T> where T is `{typeof(T).Name}`")
-        };
-    }
-
-}
 public record class ResponseDto<T>(T? Data, bool Success = true, string? ErrorMessage = null);
