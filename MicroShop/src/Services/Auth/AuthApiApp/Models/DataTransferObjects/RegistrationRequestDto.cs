20850069ebd416e8138c02d4d4469196106ea888
Terry Moreland <tsmoreland@live.com>
2024-03-22 18:15:49 +0000

    MIcroShop - Add DTOs for AuthApi

---
+++ MicroShop/src/Services/Auth/AuthApiApp/Models/DataTransferObjects/RegistrationRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroShop.Services.Auth.AuthApiApp.Models.DataTransferObjects;
+
+public sealed record class RegistrationRequestDto(
+    [property: EmailAddress] string Email,
+    [property: MinLength(2), MaxLength(10)] string Name,
+    [property: Phone] string PhoneNumber,
+    [property: MinLength(12)] string Password,
+    string ConfirmPassword) : IValidatableObject
+{
+    /// <inheritdoc />
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        List<ValidationResult> results = [];
+        Validator.TryValidateProperty(Email, validationContext, results);
+        Validator.TryValidateProperty(Name, validationContext, results);
+        Validator.TryValidateProperty(PhoneNumber, validationContext, results);
+        Validator.TryValidateProperty(Password, validationContext, results);
+
+        if (Password != ConfirmPassword)
+        {
+            results.Add(new ValidationResult("Passwords must match"));
+        }
+
+        return results;
+    }
+}
---
+++ MicroShop/src/Services/Auth/AuthApiApp/Models/DataTransferObjects/ResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MicroShop.Services.Auth.AuthApiApp.Models.DataTransferObjects;
+
+public sealed record class ResponseDto(object? Data, bool Success = true, string? ErrorMessage = null) : ResponseDto<object>(Data, Success, ErrorMessage)
+{
+    public static ResponseDto<T> Ok<T>(T value)
+    {
+        return new ResponseDto<T>(value, true, null);
+    }
+    public static ResponseDto<T> Error<T>(string errorMessage)
+    {
+        return new ResponseDto<T>(default, false, errorMessage);
+    }
+
+}
+
+public record class ResponseDto<T>(T? Data, bool Success = true, string? ErrorMessage = null);
