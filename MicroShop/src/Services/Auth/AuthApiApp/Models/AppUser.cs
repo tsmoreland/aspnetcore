ee0f6adbed03e7a0a5ae95d2cd4aaaa48d61eaa1
Terry Moreland <tsmoreland@live.com>
2024-03-22 18:16:11 +0000

    add Options and AppUser object

--- MicroShop/src/Services/Auth/AuthApiApp/Models/AppUser.cs
+++ MicroShop/src/Services/Auth/AuthApiApp/Models/AppUser.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace MicroShop.Services.Auth.AuthApiApp.Models;
 
-public sealed class AppUser : IdentityUser
+public sealed class AppUser : IdentityUser, IValidatableObject
 {
     public string Name { get; set; } = string.Empty;
+
+    /// <inheritdoc />
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        throw new NotImplementedException();
+    }
 }
