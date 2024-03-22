340b3c9f236700b407d9aa5d64030b044b12d6b8
Terry Moreland <tsmoreland@live.com>
2024-03-22 18:13:47 +0000

    MicroShop - override OnModelCreating in AppDbContext of AuthApi to set limit on Name (200 characters)

--- MicroShop/src/Services/Auth/AuthApiApp/Infrrastructure/Data/AppDbContext.cs
+++ MicroShop/src/Services/Auth/AuthApiApp/Infrrastructure/Data/AppDbContext.cs
@@ -7,4 +7,13 @@ namespace MicroShop.Services.Auth.AuthApiApp.Infrrastructure.Data;
 public sealed class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser, AppRole, string>(options)
 {
     public DbSet<AppUser> ApplicationUsers => Set<AppUser>();
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>().Property(e => e.Name).HasMaxLength(200).IsRequired();
+    }
 }
+
