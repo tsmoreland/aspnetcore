99cf3f733e0625ed2149e84c0508e5df6849fc7b
Terry Moreland <tsmoreland@live.com>
2024-03-25 20:59:11 +0000

    MicroShop - add product model

---
+++ MicroShop/src/Services/Products/ProductsApiApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace MicroShop.Services.Products.ProductsApiApp.Models;
+
+public class Product(int productId, string name, double price, string description, string categoryName, string imageUrl)
+{
+    public Product(string name, double price, string description, string categoryName, string imageUrl)
+        : this(0, name, price, description, categoryName, imageUrl)
+    {
+    }
+
+    public int ProductId { get; set; } = productId;
+    public string Name { get; set; } = name;
+    public double Price { get; set; } = price;
+    public string Description { get; set; } = description;
+    public string CategoryName { get; set; } = categoryName;
+    public string ImageUrl { get; set; } = imageUrl;
+   
+}
