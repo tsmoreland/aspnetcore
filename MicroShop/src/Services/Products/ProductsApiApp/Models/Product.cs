c69f72188324d4a7666994def1fe902cbc6c0cca
Terry Moreland <tsmoreland@live.com>
2024-03-26 19:04:22 +0000

    MicroShop - rename id parameter - add conversion constructor and method to ProductDto

--- MicroShop/src/Services/Products/ProductsApiApp/Models/Product.cs
+++ MicroShop/src/Services/Products/ProductsApiApp/Models/Product.cs
@@ -1,13 +1,13 @@
 ﻿namespace MicroShop.Services.Products.ProductsApiApp.Models;
 
-public sealed class Product(int productId, string name, double price, string? description, string categoryName, string? imageUrl)
+public sealed class Product(int id, string name, double price, string? description, string categoryName, string? imageUrl)
 {
     public Product(string name, double price, string? description, string categoryName, string? imageUrl)
         : this(0, name, price, description, categoryName, imageUrl)
     {
     }
 
-    public int ProductId { get; set; } = productId;
+    public int Id { get; set; } = id;
     public string Name { get; set; } = name;
     public double Price { get; set; } = price;
     public string? Description { get; set; } = description;
