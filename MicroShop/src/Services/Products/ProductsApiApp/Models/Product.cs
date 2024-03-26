3d99696d9e453bdf37fe280860641d26abe5edf1
Terry Moreland <tsmoreland@live.com>
2024-03-26 19:26:38 +0000

    MicroShop - add Products DbContext and entity type configuration for products - add normalized properties to simplify search

--- MicroShop/src/Services/Products/ProductsApiApp/Models/Product.cs
+++ MicroShop/src/Services/Products/ProductsApiApp/Models/Product.cs
@@ -1,17 +1,46 @@
 ﻿namespace MicroShop.Services.Products.ProductsApiApp.Models;
 
-public sealed class Product(int id, string name, double price, string? description, string categoryName, string? imageUrl)
+public sealed class Product
 {
+    private string _name;
+    private string _categoryName;
+
+    public Product(int id, string name, double price, string? description, string categoryName, string? imageUrl)
+    {
+        Id = id;
+        _name = name;
+        _categoryName = categoryName;
+        NormalizedName = _name.ToUpperInvariant();
+        NormalizedCategoryName = _categoryName.ToUpperInvariant();
+        Price = price;
+        Description = description;
+        ImageUrl = imageUrl;
+    }
+
     public Product(string name, double price, string? description, string categoryName, string? imageUrl)
         : this(0, name, price, description, categoryName, imageUrl)
     {
     }
 
-    public int Id { get; set; } = id;
-    public string Name { get; set; } = name;
-    public double Price { get; set; } = price;
-    public string? Description { get; set; } = description;
-    public string CategoryName { get; set; } = categoryName;
-    public string? ImageUrl { get; set; } = imageUrl;
-   
+    public int Id { get; set; } 
+    public string Name { get => _name; set => SetName(value); } 
+    public string NormalizedName { get; private set; }
+    public double Price { get; set; } 
+    public string? Description { get; set; } 
+    public string CategoryName { get => _categoryName; set => SetCategoryName(value); } 
+    public string NormalizedCategoryName { get; private set; }
+    public string? ImageUrl { get; set; } 
+
+    private string SetName(string? value)
+    {
+        _name = value ?? string.Empty;
+        NormalizedName = _name.ToUpperInvariant();
+        return _name;
+    }
+    private string SetCategoryName(string? value)
+    {
+        _categoryName = value ?? string.Empty;
+        NormalizedName = _categoryName.ToUpperInvariant();
+        return _categoryName;
+    }
 }
