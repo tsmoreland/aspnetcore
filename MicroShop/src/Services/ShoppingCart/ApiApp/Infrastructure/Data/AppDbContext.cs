c99d1c7b4ec0df1a01a7fb2badd115c2a9017a7f
Terry Moreland <tsmoreland@live.com>
2024-04-06 01:25:05 +0100

    MicroShop - stubbing out shopping cart api

---
+++ MicroShop/src/Services/ShoppingCart/ApiApp/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroShop.Services.ShoppingCart.ApiApp.Infrastructure.Data;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
