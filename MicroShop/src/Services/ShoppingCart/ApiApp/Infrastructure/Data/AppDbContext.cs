18e7d57d023589f70628e936a51e6ed828b18032
Terry Moreland <tsmoreland@live.com>
2024-04-06 13:11:27 +0100

    MicroShop - add cart details and headers to app db context

--- MicroShop/src/Services/ShoppingCart/ApiApp/Infrastructure/Data/AppDbContext.cs
+++ MicroShop/src/Services/ShoppingCart/ApiApp/Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MicroShop.Services.ShoppingCart.ApiApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroShop.Services.ShoppingCart.ApiApp.Infrastructure.Data;
 
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<CartDetails> CartDetails => Set<CartDetails>();
+    public DbSet<CartHeader> CartHeaders => Set<CartHeader>();
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
