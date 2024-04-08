43711b80e68db5102af92a51d9ba1c57bcc2fa1f
Terry Moreland <tsmoreland@live.com>
2024-04-08 22:40:53 +0100

    MicroShop - update Shopping Cart API to include product description in items

--- MicroShop/src/Services/ShoppingCart/ApiApp/Models/DataTransferObjects/Response/CartItemDto.cs
+++ MicroShop/src/Services/ShoppingCart/ApiApp/Models/DataTransferObjects/Response/CartItemDto.cs
@@ -4,6 +4,7 @@ public sealed record class CartItemDto(
     int Id,
     int ProductId,
     string ProductName,
+    string? ProductDescription,
     double Price,
     string? ImageUrl,
     int Count);
