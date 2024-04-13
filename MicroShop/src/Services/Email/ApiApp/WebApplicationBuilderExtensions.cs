e2d59d6154d147c44e7240320601849c7a4d690e
Terry Moreland <tsmoreland@live.com>
2024-04-13 23:57:20 +0100

    MicroShop Email Api add email api project, just simple setup so far

---
+++ MicroShop/src/Services/Email/ApiApp/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,112 @@
+﻿using System.Security.Authentication;
+using System.Text;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace MicroShop.Services.Email.ApiApp;
+
+internal static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder Configure(this WebApplicationBuilder builder)
+    {
+        IConfiguration configuration = builder.Configuration;
+        IServiceCollection services = builder.Services;
+        IHostEnvironment environment = builder.Environment;
+
+        builder.WebHost
+            .UseKestrel(static (ctx, options) =>
+            {
+                options
+                    .Configure(ctx.Configuration.GetSection("Kestrel"))
+                    .Endpoint("HTTPS", listenOptions => listenOptions.HttpsOptions.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13);
+                options.AddServerHeader = false;
+            });
+
+        if (environment.IsDevelopment())
+        {
+            services.AddProblemDetails();
+        }
+        else
+        {
+            services
+                .AddProblemDetails(static options => options.CustomizeProblemDetails = static ctx => ctx.ProblemDetails.Extensions.Clear());
+        }
+
+        services
+            .AddOptions()
+            .AddMemoryCache()
+            .AddRateLimiter(static _ => _.AddFixedWindowLimiter(policyName: "fixedWindow",
+                options =>
+                {
+                    options.PermitLimit = 500;
+                    options.Window = TimeSpan.FromSeconds(1);
+                    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    options.QueueLimit = 5;
+                }));
+
+        services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen(static options =>
+            {
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization token as `Bearer JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme,
+                            }
+                        },
+                        []
+                    }
+                });
+            })
+            .AddResponseCompression(static options =>
+            {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            })
+            .Configure<BrotliCompressionProviderOptions>(static options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+
+        services
+            .AddHttpContextAccessor()
+            .AddAuthentication(static options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                string issuer = configuration["ApiJwtOptions:Issuer"] ?? string.Empty;
+                string audience = configuration["ApiJwtOptions:Audience"] ?? string.Empty;
+                byte[] secret = Encoding.UTF8.GetBytes(configuration["ApiJwtOptions:Secret"] ?? string.Empty);
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secret),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                };
+            });
+
+        return builder;
+    }
+}
