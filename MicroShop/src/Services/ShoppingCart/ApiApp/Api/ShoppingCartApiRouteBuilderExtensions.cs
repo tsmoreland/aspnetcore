bf05592c4d76e2153ece69e322156ce545375e2d
Terry Moreland <tsmoreland@live.com>
2024-04-13 19:15:51 +0100

    MicroShop - updated MapShoppingCartApi to include Email cart

--- MicroShop/src/Services/ShoppingCart/ApiApp/Api/ShoppingCartApiRouteBuilderExtensions.cs
+++ MicroShop/src/Services/ShoppingCart/ApiApp/Api/ShoppingCartApiRouteBuilderExtensions.cs
@@ -18,6 +18,7 @@ internal static class ShoppingCartApiRouteBuilderExtensions
         return builder
             .MapUpsertCart()
             .MapGetCartByUserId()
+            .MapEmailCart()
             .MapApplyCoupon()
             .MapRemoveCoupon()
             .MapDeleteCartById();
