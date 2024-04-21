2f1892d1e50743d55790db9a05f826598afd924c
Terry Moreland <tsmoreland@live.com>
2024-04-21 02:12:40 +0100

    MicroShop FrontEnd - add Order dto's and initial service

---
+++ MicroShop/src/FrontEnd/Web.MvcApp/Services/Contracts/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using MicroShop.Web.MvcApp.Models;
+using MicroShop.Web.MvcApp.Models.Cart;
+using MicroShop.Web.MvcApp.Models.Orders;
+
+namespace MicroShop.Web.MvcApp.Services.Contracts;
+
+public interface IOrderService
+{
+    Task<ResponseDto<OrderSummaryDto>?> Create(CartSummaryDto cart, CancellationToken cancellationToken = default);
+}
---
+++ MicroShop/src/FrontEnd/Web.MvcApp/Services/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using MicroShop.Web.MvcApp.Models;
+using MicroShop.Web.MvcApp.Models.Cart;
+using MicroShop.Web.MvcApp.Models.Orders;
+using MicroShop.Web.MvcApp.Services.Contracts;
+
+namespace MicroShop.Web.MvcApp.Services;
+
+public sealed class OrderService(IBaseService baseService) : IOrderService
+{
+    private const string ClientName = "OrderApi";
+
+    /// <inheritdoc />
+    public async Task<ResponseDto<OrderSummaryDto>?> Create(CartSummaryDto cart, CancellationToken cancellationToken = default) =>
+        await SendAsync<OrderSummaryDto>(new RequestDto(ApiType.Post, "/api/orders", cart));
+
+    private Task<ResponseDto<T>?> SendAsync<T>(RequestDto data) => baseService.SendAsync<T>(ClientName, data);
+    private Task<ResponseDto?> SendAsync(RequestDto data) => baseService.SendAsync(ClientName, data);
+}
