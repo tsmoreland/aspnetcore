12c9316b4a2f0c059be2f40aa305197f7ce3ca85
Terry Moreland <tsmoreland@live.com>
2024-05-06 23:44:40 +0100

    MicroShop Rewards api - adding database infrastructure

---
+++ MicroShop/src/Services/Rewards/RestApi.App/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microshop.Services.Rewards.RestApi.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microshop.Services.Rewards.RestApi.App.Infrastructure.Data;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Reward> Rewards => Set<Reward>();
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
---
+++ MicroShop/src/Services/Rewards/RestApi.App/Models/Reward.cs
@@ -0,0 +1,26 @@
+﻿namespace Microshop.Services.Rewards.RestApi.App.Models;
+
+public sealed class Reward
+{
+    private Reward(int id, string userId, DateTime earnedDate, int rewardPoints, int orderId)
+    {
+        Id = id;
+        UserId = userId;
+        EarnedDate = earnedDate;
+        RewardPoints = rewardPoints;
+        OrderId = orderId;
+    }
+
+    public Reward(string userId, DateTime earnedDate, int rewardPoints, int orderId)
+        : this(0, userId, earnedDate, rewardPoints, orderId)
+    {
+        // TODO: validation on paramters
+    }
+
+    public int Id { get; private init; }
+    public string UserId { get; private init;  }
+    public DateTime EarnedDate { get; private init; }
+    public int RewardPoints { get; private init; }
+    public int OrderId { get; private init; }
+
+}
