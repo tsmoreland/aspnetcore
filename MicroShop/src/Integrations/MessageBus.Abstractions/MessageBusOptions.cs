4bb694eda0e5fb2b6c6d00b0eb34e663fb295432
Terry Moreland <tsmoreland@live.com>
2024-05-11 16:23:35 +0100

    MicroShop Integrations - update message bus abstractions adding TopicName to options

--- MicroShop/src/Integrations/MessageBus.Abstractions/MessageBusOptions.cs
+++ MicroShop/src/Integrations/MessageBus.Abstractions/MessageBusOptions.cs
@@ -3,7 +3,7 @@
 public class MessageBusOptions
 {
     public MessageBusType BusType { get; set; } = MessageBusType.Azure;
-
     public string QueueName { get; set; } = string.Empty;
+    public string TopicName { get; set; } = string.Empty;
 }
 
