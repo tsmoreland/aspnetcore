0bc4650f4b999fc601237f0b7f7d0dd755b612af
Terry Moreland <tsmoreland@live.com>
2024-05-12 22:35:15 +0100

    MicroShop Orders - get orders by user id added

---
+++ MicroShop/src/Services/Orders/ApiApp/Features/Queries/GetOrdersByUserId/GetOrdersByUserIdRequest.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using MicroShop.Services.Orders.ApiApp.Models;
+
+namespace MicroShop.Services.Orders.ApiApp.Features.Queries.GetOrdersByUserId;
+
+public sealed record class GetOrdersByUserIdRequest(string UserId) : IStreamRequest<OrderHeader>;
---
+++ MicroShop/src/Services/Orders/ApiApp/Features/Queries/GetOrdersByUserId/GetOrdersByUserIdRequestHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MicroShop.Services.Orders.ApiApp.Infrastructure.Data;
+using MicroShop.Services.Orders.ApiApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroShop.Services.Orders.ApiApp.Features.Queries.GetOrdersByUserId;
+
+public sealed class GetOrdersByUserIdRequestHandler(AppDbContext dbContext) : IStreamRequestHandler<GetOrdersByUserIdRequest, OrderHeader>
+{
+    /// <inheritdoc />
+    public IAsyncEnumerable<OrderHeader> Handle(GetOrdersByUserIdRequest request, CancellationToken cancellationToken)
+    {
+        string userId = request.UserId;
+        return dbContext.OrderHeaders.AsNoTracking().Where(e => e.UserId == userId).AsAsyncEnumerable();
+    }
+}
