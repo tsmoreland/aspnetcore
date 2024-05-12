6a7e285741a9a506230c346d506d878926fc5914
Terry Moreland <tsmoreland@live.com>
2024-05-12 22:35:31 +0100

    MicroShop  Orders - get all orders added

---
+++ MicroShop/src/Services/Orders/ApiApp/Features/Queries/GetOrders/GetOrderRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MicroShop.Services.Orders.ApiApp.Infrastructure.Data;
+using MicroShop.Services.Orders.ApiApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroShop.Services.Orders.ApiApp.Features.Queries.GetOrders;
+
+public sealed class GetOrderRequestHandler(AppDbContext dbContext) : IStreamRequestHandler<GetOrdersRequest, OrderHeader>
+{
+    /// <inheritdoc />
+    public IAsyncEnumerable<OrderHeader> Handle(GetOrdersRequest request, CancellationToken cancellationToken)
+    {
+        return dbContext.OrderHeaders.AsNoTracking().AsAsyncEnumerable();
+    }
+}
---
+++ MicroShop/src/Services/Orders/ApiApp/Features/Queries/GetOrders/GetOrdersRequest.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using MicroShop.Services.Orders.ApiApp.Models;
+
+namespace MicroShop.Services.Orders.ApiApp.Features.Queries.GetOrders;
+
+public sealed record class GetOrdersRequest : IStreamRequest<OrderHeader>;
