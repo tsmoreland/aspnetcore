79ecce154a2da71aa46511b99dca4601ea13ef4c
Terry Moreland <tsmoreland@live.com>
2024-05-27 13:56:15 +0100

    MicroShop FrontEnd corrections and updates to order model

--- MicroShop/src/FrontEnd/Web.MvcApp/Models/Orders/OrderDto.cs
+++ MicroShop/src/FrontEnd/Web.MvcApp/Models/Orders/OrderDto.cs
@@ -1,3 +1,5 @@
 ﻿namespace MicroShop.Web.MvcApp.Models.Orders;
 
-public sealed record class OrderDto(int Id, string UserId, OrderStatus Status, string Name, string Email, double OrderTotal, IEnumerable<OrderItemDto> Items);
+public sealed record class OrderDto(int Id, string UserId, OrderStatus Status, string Name, string Email,
+    double OrderTotal, DateTime OrderTime, string? StripeSessionId, string? PaymentIntentId,
+    IEnumerable<OrderItemDto> Items);
