d5a212946f5126ebf5747d2a67a9670fe495f449
Terry Moreland <tsmoreland@live.com>
2024-06-03 18:26:13 +0100

    MicroShop Gateway - restructure app - add auth details in settings - add nuget package references - configure authentication and ocelot gateway

--- MicroShop/src/Gateway/App/Program.cs
+++ MicroShop/src/Gateway/App/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MicroShop.Gateway.App;
+using Serilog;
 
-app.MapGet("/", () => "Hello World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-app.Run();
+try
+{
+    WebApplicationBuilder builder = WebApplication
+        .CreateBuilder(args)
+        .Configure();
+
+    builder.Host
+        .UseSerilog((context, loggerConfiguration) => loggerConfiguration
+#if DEBUG
+            .WriteTo.Console()
+#endif
+            .ReadFrom.Configuration(context.Configuration));
+
+    WebApplication app = builder
+        .Build()
+        .ConfigurePipeline();
+
+    app.Run();
+}
+catch (HostAbortedException) when (args is ["--applicationName", _])
+{
+    // expected during EF Core Add Migration
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown complete.");
+    Log.CloseAndFlush();
+}
+
