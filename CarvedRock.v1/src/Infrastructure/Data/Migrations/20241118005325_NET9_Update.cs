9cb01da4277aba5f03e9d5d25106e7101ba01071
Terry Moreland <terry.s.moreland@gmail.com>
2024-11-18 00:54:23 +0000

    Migration CarvedRock to .NET9

---
+++ CarvedRock.v1/src/Infrastructure/Data/Migrations/20241118005325_NET9_Update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarvedRock.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NET9_Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "item_type",
+                table: "items",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "UncatagorizedItem",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldDefaultValue: "UncatagorizedItem");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "item_type",
+                table: "items",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "UncatagorizedItem",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21,
+                oldDefaultValue: "UncatagorizedItem");
+        }
+    }
+}
