44b3f279d0f400e38888450116730cceefe3659b
Terry Moreland <terry.s.moreland@gmail.com>
2024-11-25 01:00:40 +0000

    GloboTicket.Shop - update references

--- GloboTicket.Shop/src/Shared.Api/Filters/AddModelStateRequestFilter.cs
+++ GloboTicket.Shop/src/Shared.Api/Filters/AddModelStateRequestFilter.cs
@@ -1,24 +1,5 @@
-﻿//
-// Copyright © 2023 Terry Moreland
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GloboTicket.Shop.Shared.Api.Models;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GloboTicket.Shop.Shared.Api.Filters;
 
@@ -27,7 +8,7 @@ public sealed class AddModelStateFeatureFilter : IAsyncActionFilter
     /// <inheritdoc />
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        ModelStateDictionary modelState = context.ModelState;
+        var modelState = context.ModelState;
         context.HttpContext.Features.Set(new ModelStateContainer(modelState));
         await next();
     }
